c731a1967fe93314166e29d275812f217b01dd38
azidahrashid <turnkeyjie@gmail.com>
2025-12-30 14:50:39 +0800

    restructure index
    update code

---
+++ js/include.js
@@ -0,0 +1,13 @@
+async function loadPartial(id, path) {
+  const res = await fetch(path);
+  if (!res.ok) {
+    console.error(`Failed to load ${path}`);
+    return;
+  }
+  document.getElementById(id).innerHTML = await res.text();
+}
+
+// index.html
+loadPartial("front-login", "pages/login.html");
+loadPartial("nav", "partials/nav.html");
+loadPartial("footer", "partials/footer.html");
